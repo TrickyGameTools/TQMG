c65d73e948e5e46e27be60bc4ec630c2cdf34a5a
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-17 12:00:56 +0100

    Lic block + basic config

--- TQMG.cs
+++ TQMG.cs
@@ -1,4 +1,20 @@
-namespace TrickyUnits{
+// Lic:
+// TQMG.cs
+// (c)  Jeroen Petrus Broks.
+// 
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at
+// http://mozilla.org/MPL/2.0/.
+// Version: 19.03.17
+// EndLic
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using UseJCR6;
+
+namespace TrickyUnits
+{
 
 #region image
 class TQMGImage{
@@ -8,15 +24,38 @@ class TQMGImage{
 #region text
 class TQMGFont{
 }
-#endregion
+
+    #endregion
 
 
-class TQMG{
-      public void Init(){
-           MKL.Lic("","");
-           MKL.Version("","");
-      }
-}
+
+
+    #region core
+    class Class_TQMG {
+
+        readonly GraphicsDeviceManager gfxm;
+        readonly SpriteBatch spriteBatch;
+        readonly TJCRDIR ajcr;
+        readonly GraphicsDevice gfxd;
+
+        public Class_TQMG(GraphicsDeviceManager agfxm, GraphicsDevice agfxd, SpriteBatch aSB, TJCRDIR ajcr) {
+            #region MKL
+            MKL.Lic("TQMG - TQMG.cs", "Mozilla Public License 2.0");
+            MKL.Version("TQMG - TQMG.cs", "19.03.17");
+            #endregion
+
+            #region TQMG core setup
+            gfxm = agfxm;
+            gfxd = agfxd;
+            spriteBatch = aSB;
+            #endregion
+        }
+
+    }
+
+    #endregion
+
+
 
 
 }
