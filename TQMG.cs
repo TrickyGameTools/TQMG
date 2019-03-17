33ebbee75fe258c490cbaf0e09d4c6647dbf2570
Jeroen Broks <facebook@tbbs.nl>
2019-03-17 11:28:56 +0100

    Create TQMG.cs
    Just a base setup

---
+++ TQMG.cs
@@ -0,0 +1,22 @@
+namespace TrickyUnits{
+
+#region image
+class TQMGImage{
+}
+#endregion
+
+#region text
+class TQMGFont{
+}
+#endregion
+
+
+class TQMG{
+      public void Init(){
+           MKL.Lic("","");
+           MKL.Version("","");
+      }
+}
+
+
+}
